bed322b6b6a586625d3e366935b1692acf409a10
Adpro <101449500+adproqwq@users.noreply.github.com>
2024-02-02 22:05:57 +0800

    fix: readme.md 样式丢失

--- script/update.js
+++ script/update.js
@@ -5,7 +5,7 @@ export const writeReadMeMd = async() => {
   const subListJSON = JSON.parse(listFile);
   let list = ``;
   for(let i in subListJSON){
-    list += `- [${subListJSON[i].name}](https://github.com/${subListJSON[i].repo}/tree/${subListJSON[i].branch})<br>`;
+    list += `- [${subListJSON[i].name}](https://github.com/${subListJSON[i].repo}/tree/${subListJSON[i].branch})<br>\n`;
   }
   const mdTemplate = await fs.readFile(process.cwd() + '/Template.md', 'utf-8');
   const readMeMdText = mdTemplate.replaceAll('--thirdPartySubList--', list);
