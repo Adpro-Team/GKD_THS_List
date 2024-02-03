27c5b57488f6fcae16e5e8ec771c3b0b0f17ef0e
Adpro <101449500+adproqwq@users.noreply.github.com>
2024-02-03 23:29:35 +0800

    feat: author

--- script/update.js
+++ script/update.js
@@ -5,11 +5,23 @@ export const writeReadMeMd = async() => {
   const subListJSON = JSON.parse(listFile);
   let list = ``;
   for(let i in subListJSON){
+    if(subListJSON[i].name == ''){
+      throw new Error('The value of name is empty');
+      continue
+    }
+    if(subListJSON[i].author == ''){
+      throw new Error('The value of author is empty');
+      continue
+    }
+    if(subListJSON[i].subUrl == ''){
+      throw new Error('Can not find the subscription import url');
+      continue
+    }
     if(subListJSON[i].repo  != '' && subListJSON[i].branch != ''){
-      list += `- [${subListJSON[i].name}](https://github.com/${subListJSON[i].repo}/tree/${subListJSON[i].branch})<br>\n`;
+      list += `- [${subListJSON[i].name}](https://github.com/${subListJSON[i].repo}/tree/${subListJSON[i].branch}) By ${subListJSON[i].author}<br>\n`;
     }
     else if(subListJSON[i].repo  == '' && subListJSON[i].branch == ''){
-      list += `- *[${subListJSON[i].name}](${subListJSON[i].subUrl})<br>\n`;
+      list += `- *[${subListJSON[i].name}](${subListJSON[i].subUrl}) By ${subListJSON[i],author}<br>\n`;
     }
     else{
       throw new Error('The values of repo and branch must be empty or completed at the same time');
