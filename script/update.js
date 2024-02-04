e89a52c1d6591c0dd92645fef0c40d07fb7c8b89
Adpro <101449500+adproqwq@users.noreply.github.com>
2024-02-05 00:11:17 +0800

    feat: active

--- script/update.js
+++ script/update.js
@@ -5,27 +5,17 @@ export const writeReadMeMd = async() => {
   const subListJSON = JSON.parse(listFile);
   let list = ``;
   for(let i in subListJSON){
-    if(subListJSON[i].name == ''){
-      throw new Error('The value of name is empty');
-      continue
-    }
-    if(subListJSON[i].author == ''){
-      throw new Error('The value of author is empty');
-      continue
-    }
-    if(subListJSON[i].subUrl == ''){
-      throw new Error('Can not find the subscription import url');
-      continue
-    }
     if(subListJSON[i].repo  != '' && subListJSON[i].branch != ''){
-      list += `- [${subListJSON[i].name}](https://github.com/${subListJSON[i].repo}/tree/${subListJSON[i].branch}) By ${subListJSON[i].author}<br>\n`;
+      if(subListJSON[i].active == false){
+        list += `- ~~[${subListJSON[i].name}](https://github.com/${subListJSON[i].repo}/tree/${subListJSON[i].branch}) By ${subListJSON[i].author}~~<br>\n`;
+      }
+      else list += `- [${subListJSON[i].name}](https://github.com/${subListJSON[i].repo}/tree/${subListJSON[i].branch}) By ${subListJSON[i].author}<br>\n`;
     }
     else if(subListJSON[i].repo  == '' && subListJSON[i].branch == ''){
-      list += `- *[${subListJSON[i].name}](${subListJSON[i].subUrl}) By ${subListJSON[i],author}<br>\n`;
-    }
-    else{
-      throw new Error('The values of repo and branch must be empty or completed at the same time');
-      continue
+      if(subListJSON[i].active == false){
+        list += `- *~~[${subListJSON[i].name}](${subListJSON[i].subUrl}) By ${subListJSON[i],author}~~<br>\n`;
+      }
+      else list += `- *[${subListJSON[i].name}](${subListJSON[i].subUrl}) By ${subListJSON[i],author}<br>\n`;
     }
   }
   const mdTemplate = await fs.readFile(process.cwd() + '/Template.md', 'utf-8');
