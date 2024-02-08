d2690ee37c6aa930cf754538f5d4fa8938a0b453
Adpro <101449500+adproqwq@users.noreply.github.com>
2024-02-08 21:16:32 +0800

    fix: 样式

--- script/update.js
+++ script/update.js
@@ -3,8 +3,9 @@ import fs from 'node:fs/promises';
 export const writeReadMeMd = async() => {
   let listFile = await fs.readFile(process.cwd() + '/list.json', 'utf-8');
   const subListJSON = JSON.parse(listFile);
-  let list = `- `;
+  let list = ``;
   for(let i in subListJSON){
+    list += '- '
     if(subListJSON[i].active == false) list += '*';
     if(subListJSON[i].repo == '') list += `${subListJSON[i].name}`;
     else list += `[${subListJSON[i].name}](https://github.com/${subListJSON[i].repo}/tree/${subListJSON[i].branch})`;
@@ -13,7 +14,7 @@ export const writeReadMeMd = async() => {
       const urlName = subListJSON[i].subUrls[j].hasOwnProperty('name') ? subListJSON[i].subUrls[j].name : '订阅链接';
       list += `[${urlName}](${subListJSON[i].subUrls[j].importUrl}) `;
     }
-    list +='\r- ';
+    list +='\r';
   }
   const mdTemplate = await fs.readFile(process.cwd() + '/Template.md', 'utf-8');
   const readMeMdText = mdTemplate.replaceAll('--thirdPartySubList--', list);
