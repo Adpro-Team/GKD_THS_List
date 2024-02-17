e1957cf3aad73b68130950b66cbc37dfa20b48b7
Adpro <101449500+adproqwq@users.noreply.github.com>
2024-02-17 22:19:46 +0800

    fix: style

--- script/update.js
+++ script/update.js
@@ -10,12 +10,12 @@ export const writeReadMeMd = async() => {
     let repoUrl = repository != null && branch != null ? `https://github.com/${subListJSON[i].repo}/tree/${subListJSON[i].branch}` : null;
     let repoText = repoUrl == null ? '无' : subListJSON[i].repo;
     list += `|${subListJSON[i].name}|${subListJSON[i].author}|`;
-    list += repoText == '无' ? `${repoText}|` : `[${repoText}](${repoUrl})|`;
+    list += repoText == '无' ? `${repoText}|` : `[${repoText}](${repoUrl})|<ol>`;
     for(let j in subListJSON[i].subUrls){
       const urlName = subListJSON[i].subUrls[j].hasOwnProperty('name') ? subListJSON[i].subUrls[j].name : '订阅链接';
-      list += `- [${urlName}](${subListJSON[i].subUrls[j].importUrl})<br>`;
+      list += `<li>[${urlName}](${subListJSON[i].subUrls[j].importUrl})</li><br>`;
     }
-    list += subListJSON[i].active == true ? '|仍在维护|' : '|停止维护|';
+    list += subListJSON[i].active == true ? '</ol>|仍在维护|' : '</ol>|停止维护|';
     list +='\r';
   }
   const mdTemplate = await fs.readFile(process.cwd() + '/Template.md', 'utf-8');
