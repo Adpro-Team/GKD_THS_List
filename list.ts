0ee16d347cc1572e015bc8f4651859619f01ea38
Adpro <101449500+adproqwq@users.noreply.github.com>
2024-02-24 00:27:43 +0800

    feat: 添加国内镜像链接

--- list.ts
+++ list.ts
@@ -1,99 +1,104 @@
-import { Root } from "./scripts/types"
+import { Root } from './scripts/types'
 
 export const List: Root = [
   {
-    name: "Adpro的GKD订阅",
-    author: "Adpro",
+    name: 'Adpro的GKD订阅',
+    author: 'Adpro',
     id: 825,
     subUrls: [
       {
-        importUrl: "https://raw.gitmirror.com/Adpro-Team/GKD_subscription/main/dist/Adpro_gkd.json5",
+        importUrl: 'https://raw.gitmirror.com/Adpro-Team/GKD_subscription/main/dist/Adpro_gkd.json5',
       },
     ],
-    repo: "Adpro-Team/GKD_subscription",
-    branch: "main",
+    repo: 'Adpro-Team/GKD_subscription',
+    branch: 'main',
     active: true,
   },
   {
-    name: "AIsouler的GKD订阅",
-    author: "AIsouler",
+    name: 'AIsouler的GKD订阅',
+    author: 'AIsouler',
     id: 666,
     subUrls: [
       {
-        importUrl: "https://raw.gitmirror.com/AIsouler/GKD_subscription/main/dist/AIsouler_gkd.json5",
+        importUrl: 'https://raw.gitmirror.com/AIsouler/GKD_subscription/main/dist/AIsouler_gkd.json5',
       },
     ],
-    repo: "AIsouler/GKD_subscription",
-    branch: "main",
+    repo: 'AIsouler/GKD_subscription',
+    branch: 'main',
     active: true,
   },
   {
-    name: "奥怪的GKD订阅",
-    author: "aoguai",
+    name: '奥怪的GKD订阅',
+    author: 'aoguai',
     id: 86,
     subUrls: [
       {
-        name: "Github源",
-        importUrl: "https://raw.githubusercontent.com/aoguai/subscription/custom/dist/aoguai_gkd.json5",
+        name: 'Github源',
+        importUrl: 'https://raw.githubusercontent.com/aoguai/subscription/custom/dist/aoguai_gkd.json5',
       },
       {
-        name: "npmmirror源",
-        importUrl: "https://registry.npmmirror.com/@aoguai/subscription/latest/files",
+        name: 'npmmirror源',
+        importUrl: 'https://registry.npmmirror.com/@aoguai/subscription/latest/files',
       },
       {
-        name: "gitmirror源",
-        importUrl: "https://raw.gitmirror.com/aoguai/subscription/custom/dist/aoguai_gkd.json5",
+        name: 'gitmirror源（国内免翻）',
+        importUrl: 'https://raw.gitmirror.com/aoguai/subscription/custom/dist/aoguai_gkd.json5',
       },
       {
-        name: "Cloudflare源",
-        importUrl: "https://gkd.aoguai.top/",
+        name: 'Cloudflare源',
+        importUrl: 'https://gkd.aoguai.top/',
       },
       {
-        name: "jsDelivr源",
-        importUrl: "https://cdn.jsdelivr.net/gh/aoguai/subscription@custom/dist/aoguai_gkd.json5",
+        name: 'jsDelivr源',
+        importUrl: 'https://cdn.jsdelivr.net/gh/aoguai/subscription@custom/dist/aoguai_gkd.json5',
       },
     ],
-    repo: "aoguai/subscription",
-    branch: "custom",
+    repo: 'aoguai/subscription',
+    branch: 'custom',
     active: true,
   },
   {
-    name: "甘霖的GKD订阅",
-    author: "ganlinte",
+    name: '甘霖的GKD订阅',
+    author: 'ganlinte',
     id: 233,
     subUrls: [
       {
-        importUrl: "https://raw.githubusercontent.com/ganlinte/GKD-subscription/main/dist/ganlin_gkd.json5",
+        name: 'Github源',
+        importUrl: 'https://raw.githubusercontent.com/ganlinte/GKD-subscription/main/dist/ganlin_gkd.json5',
+      },
+      {
+        name: '国内镜像',
+        importUrl: 'https://raw.gitmirror.com/ganlinte/GKD-subscription/main/dist/ganlin_gkd.json5',
       },
     ],
-    repo: "ganlinte/GKD-subscription",
-    branch: "main",
+    repo: 'ganlinte/GKD-subscription',
+    branch: 'main',
     active: true,
   },
   {
-    name: "Jamison Leo的GKD订阅",
-    author: "Jamison Leo",
+    name: 'Jamison Leo的GKD订阅',
+    author: 'Jamison Leo',
     id: 1025,
     subUrls: [
       {
-        importUrl: "https://raw.gitmirror.com/JamisonLeo/GKD-subscription/main/subscription.json",
+        importUrl: 'https://raw.gitmirror.com/JamisonLeo/GKD-subscription/main/subscription.json',
       },
     ],
-    repo: "JamisonLeo/GKD-subscription",
-    branch: "main",
+    repo: 'JamisonLeo/GKD-subscription',
+    branch: 'main',
     active: true,
   },
   {
-    name: "YaChengMu的GKD订阅",
-    author: "YaChengMu",
+    name: 'YaChengMu的GKD订阅',
+    author: 'YaChengMu',
     id: 303,
     subUrls: [
       {
-        importUrl: "https://raw.gitmirror.com/YaChengMu/subscription/main/dist/gkd.json5",
+        importUrl: 'https://raw.gitmirror.com/YaChengMu/subscription/main/dist/gkd.json5',
       },
     ],
-    repo: "YaChengMu/subscription",
-    branch: "main",
+    repo: 'YaChengMu/subscription',
+    branch: 'main',
     active: true,
   },
 ];
