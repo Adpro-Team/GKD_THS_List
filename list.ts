a966bc82b38a6905c6355389e94aeb97593c32df
Adpro <101449500+adproqwq@users.noreply.github.com>
2025-02-10 18:35:36 +0800

    docs: 部分订阅维护状态更新

--- list.ts
+++ list.ts
@@ -18,7 +18,7 @@ const List: Root = [
     ],
     repo: 'Adpro-Team/GKD_subscription',
     branch: 'main',
-    active: true,
+    active: false,
   },
   {
     name: 'AIsouler的GKD订阅',
@@ -39,7 +39,7 @@ const List: Root = [
     ],
     repo: 'AIsouler/GKD_subscription',
     branch: 'main',
-    active: true,
+    active: false,
   },
   {
     name: '甘霖的GKD订阅',
